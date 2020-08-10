2b5e8a41e408df382a791c65224d2f88c742c438
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2020-08-10 08:59:46 +0530

    Ready A Bit

---
+++ src/env.js
@@ -0,0 +1,11 @@
+export const config = {
+    ENDPOINT: "https://v1.nocodeapi.com/betauser/uptime/dHmQOXeWbdNuJsII?monitors=784130459-784130462",
+
+    title: "NoCodeAPI",
+
+    website: "https://nocodeapi.com",
+
+    twitterHandle: "@nocodeapi",
+    
+    twitterURL: "https://twitter.com/nocodeapi"
+}
